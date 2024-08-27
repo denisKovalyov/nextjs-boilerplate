bdedb591948332af7951451ebcdd1ce7595525ee
Denys Kovalyov <kovalovdenis@gmail.com>
2024-08-27 17:16:47 +0300

    Refactor email api

--- src/lib/actions/email/send-email.ts
+++ src/lib/actions/email/send-email.ts
@@ -1,26 +1,5 @@
-import { ReactElement } from 'react';
-import { emailProvider, emailSendFrom } from './email-provider';
+import { emailApiProvider, type SendEmailParameters } from '@/lib/email-api';
 
-const FROM_TITLE = process.env.APP_NAME;
-
-type SendEmailParameters = {
-  email: string;
-  subject: string;
-  body: ReactElement;
-};
-
-export async function sendEmail({ email, subject, body }: SendEmailParameters) {
-  const { data, error } = await emailProvider.emails.send({
-    from: `"${FROM_TITLE}" <${emailSendFrom}>`,
-    to: email,
-    subject: `${FROM_TITLE}: ${subject}`,
-    react: body,
-  });
-
-  if (error) {
-    console.error('Failed to send email:', error);
-    throw error;
-  }
-
-  return data;
+export async function sendEmail(params: SendEmailParameters) {
+  return await emailApiProvider.sendEmail(params);
 }
---
+++ src/lib/email-api.ts
@@ -0,0 +1,31 @@
+import { ReactElement } from 'react';
+import { Resend } from 'resend';
+
+const emailProvider = new Resend(process.env.RESEND_API_KEY);
+const emailSendFrom = `support@${process.env.APP_DOMAIN_NAME}`;
+const FROM_TITLE = process.env.APP_NAME;
+
+export type SendEmailParameters = {
+  email: string;
+  subject: string;
+  body: ReactElement;
+};
+
+export const emailApiProvider = {
+  sendEmail: async ({ email, subject, body }: SendEmailParameters) => {
+    const { data, error } = await emailProvider.emails.send({
+      from: `"${FROM_TITLE}" <${emailSendFrom}>`,
+      to: email,
+      subject: `${FROM_TITLE}: ${subject}`,
+      react: body,
+    });
+    console.log('data', data);
+    if (error) {
+      console.error('Failed to send email:', error);
+      throw error;
+    }
+
+    return data;
+  }
+};
+
